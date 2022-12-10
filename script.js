898f97e7f71020890a4d1c71edcdee00bb16d90e
Pranav Naringrekar <46398819+orangeX6@users.noreply.github.com>
2022-12-10 14:28:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://the-secret-chat.onrender.com');
 
 const messageForm = document.getElementById('send');
 const messageInput = document.getElementById('message');
